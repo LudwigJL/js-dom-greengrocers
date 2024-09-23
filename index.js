1f76882bc9beae99c3708dd4cdbec98e8fa9b946
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-09-23 16:30:53 +0200

    git push

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const state = {
   items: [
     {
@@ -53,3 +54,108 @@ const state = {
   ],
   cart: []
 };
+
+const allItems = document.querySelector(".store--item-list")
+const cartItems = document.querySelector(".cart--item-list")
+
+function renderCart() {
+  cartItems.innerHTML = ''
+
+  for(let i = 0; i < state.cart.length; i++){
+    const item = state.cart[i]
+    const itemLi = document.createElement('li')
+
+    allItems.appendChild(itemLi)
+
+  }
+
+}
+
+function renderItems() {
+  allItems.innerHTML = ''
+
+  for(let i = 0; i < state.items.length; i++){
+    const item = state.items[i]
+    const itemLi = document.createElement('li')
+
+    itemLi.setAttribute('id', item.id)
+    
+    //Creating image
+    let image = document.createElement('img')
+    image.src = `assets/icons/${item.id}.svg`
+    
+    //Creating button
+    let button = document.createElement('button')
+    button.innerHTML = 'Add to cart'
+    button.onclick = () => addToCart(item)
+
+    //append image and button to itemLi
+    itemLi.appendChild(image)
+    itemLi.appendChild(button)
+
+    //append item to allItems
+    allItems.appendChild(itemLi)
+  }
+}
+
+function addToCart(item) {
+  const cartLi = document.createElement('li')
+
+  cartLi.setAttribute('id', item.id)
+
+  //Create image
+  let image = document.createElement('img')
+  image.src = `assets/icons/${item.id}.svg`
+
+  //Create removeButton
+  let removeButton = document.createElement('button')
+  removeButton.className = 'quantity-btn remove-btn center'
+  removeButton.onclick = () => removeFromCart(item.id)
+
+  //Create quantitySpan
+  let quantitySpan = document.createElement('span')
+  quantitySpan.className = 'quantity-text center'
+  quantitySpan.innerHTML = 1
+
+  //Create addButton
+  let addButton = document.createElement('button')
+  addButton.className = 'quantity-btn add-btn center'
+  addButton.onclick = () => quantitySpan.innerHTML++;
+
+  cartLi.appendChild(image)
+  cartLi.appendChild(removeButton)
+  cartLi.appendChild(quantitySpan)
+  cartLi.appendChild(addButton)
+
+
+  cartItems.appendChild(cartLi)
+}
+
+function addToCart(id){
+  const cartLi = document.getElementById(id)
+  const quantitySpan = cartLi.querySelector('.quantity-text')
+
+  //quantitySpan ++;
+
+
+  
+}
+
+function removeFromCart(id){
+  const cartLi = document.getElementById(id)
+  const quantitySpan = cartLi.querySelector('.quantity-text')
+  console.log(quantitySpan.innerHTML)
+
+  //if quantityspan > 1 just --
+
+
+  allItems.removeChild(cartLi)
+  renderCart();
+}
+
+function main() {
+    renderItems()
+    renderCart()
+}
+
+main()
